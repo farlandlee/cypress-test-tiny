178d2c950da514498a110565b781f9241298a5e3
Matthew Lee <matthew@farlandlee.com>
2023-01-06 17:22:58 -0500

    failing test

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -3,4 +3,22 @@ describe('page', () => {
   it('works', () => {
     cy.visit('https://example.cypress.io')
   })
+
+  it('should have the clipboard permissions granted', () => {
+    cy.visit('https://example.cypress.io')
+    if(Cypress.browser.name === "chrome") {
+      cy.wrap(Cypress.automation('remote:debugger:protocol', {
+          command: 'Browser.grantPermissions',
+          params: { 
+              permissions: ['clipboardReadWrite', 'clipboardSanitizedWrite'], 
+              origin: "http://localhost:3000",
+          },
+      }))
+      cy.window().its('navigator.permissions')
+          .invoke('query', { name: 'clipboard-read' })
+          .then(result => {
+              expect(result.state).to.equal('granted')
+          })
+    }
+  })
 })
